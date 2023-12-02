f90d7097dc42e7286dfb71b44c7af74ee55c93c4
yusufyildirim01 <yusuf.lest@gmail.com>
2023-12-02 15:41:08 -0500

    modified:   bioboss.py

--- bioboss.py
+++ bioboss.py
@@ -9,6 +9,7 @@ import sqlite3
 app = Flask(__name__)
 
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
