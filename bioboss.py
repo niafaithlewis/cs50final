439b1cb335ec2aae517fd5ad025a73d82106b190
niafaithlewis <144560353+niafaithlewis@users.noreply.github.com>
2023-12-03 18:42:21 -0500

    Update bioboss.py

--- bioboss.py
+++ bioboss.py
@@ -14,12 +14,14 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+@app.route("/")
+def index():
+    return render_template("index.html")
+
 def get_db_connection():
     conn =sqlite3.connect('bioboss_db.sqlite')
     conn.row_factory = sqlite3.Row
     return conn
-
-@app.route("/")
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -80,6 +82,9 @@ def login():
 def logout():
     session.clear()
     return redirect("/")
+
+
+
 
 
 
